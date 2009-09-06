46095e716986d866c6b97f3506041b238e5c9ce4
Jason Dixon <jason@dixongroup.net>
2009-09-06 23:42:49 +0000

    use startup.pl instead, add stanza for Blogsum packages

---
+++ startup.pl
@@ -0,0 +1,26 @@
+
+# Blogsum
+# Copyright (c) 2009 Jason Dixon <jason@dixongroup.net>
+# All rights reserved.
+
+
+use CGI;
+use DBI;
+use DBD::SQLite;
+use HTML::Template;
+use XML::RSS;
+use Net::SMTP;
+use Captcha::reCAPTCHA;
+use HTML::Entities;
+use HTTP::Request::Common;
+use URI::_foreign;
+use Errno;
+use Net::HTTP;
+use IO::Select;
+use Compress::Zlib;
+use Log::Agent;
+use URI::http;
+use LWP::Protocol::http;
+use Carp::Heavy;
+
+1;
