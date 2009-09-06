e2c802c8813f1444a9a7faac84193529cd1b6fd1
Jason Dixon <jason@dixongroup.net>
2009-09-06 23:46:59 +0000

    move configs to a separate package

---
+++ Blogsum/Config.pm
@@ -0,0 +1,34 @@
+
+# Blogsum
+# Copyright (c) 2009 Jason Dixon <jason@dixongroup.net>
+# All rights reserved.
+
+
+###########################
+# pragmas                 #
+###########################
+package Blogsum::Config;
+use strict;
+
+
+###########################
+# user options            #
+###########################
+our $database = 'data/site.db';
+our $tmplfile_index = 'templates/index.html';
+our $tmplfile_admin = 'templates/admin.html';
+our $blog_title = 'example.com';
+our $blog_subtitle = 'My New Blog';
+our $blog_url = 'http://www.example.com/';
+our $blog_owner = 'user@example.com';
+our $blog_rights = 'Copyright 2009, Example User';
+our $feed_updates = 'hourly';
+our $captcha_pubkey = '';
+our $captcha_seckey = '';
+our $comment_max_length = '1000';
+our $comments_allowed = 0;
+our $smtp_server = 'localhost:25';
+our $smtp_sender = 'blogsum@example.com';
+
+1;
+
